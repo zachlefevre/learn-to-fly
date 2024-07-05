fde8e13ba1fb3d5deb391e0952a540b00137ac15
Zlef <me@zacharylefevre.com>
2024-07-04 23:06:20 -0500

    write getters

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -7,6 +7,10 @@ pub struct Simulation {
 }
 
 impl Simulation {
+    fn world(&self) -> &World {
+        &self.world
+    }
+
     fn random(rng: &mut dyn RngCore) -> Self {
         Self {
             world: World::random(rng)
@@ -20,6 +24,15 @@ pub struct World {
 }
 
 impl World {
+
+    fn animals(&self) -> &[Animal] {
+        &self.animals
+    }
+
+    fn foods(&self) -> &[Food] {
+        &self.foods
+    }
+
     fn random(rng: &mut dyn RngCore) -> Self {
         Self {
             animals: (0..40).map(|_| Animal::random(rng)).collect(),
@@ -36,6 +49,15 @@ struct Animal {
 }
 
 impl Animal {
+
+    fn position(&self) -> &na::Point2<f32> {
+        &self.position
+    }
+
+    fn rotation(&self) -> &na::Rotation2<f32> {
+        &self.rotation
+    }
+
     fn random(rng: &mut dyn RngCore) -> Self {
         Self {
             position: rng.gen(),
@@ -50,6 +72,11 @@ struct Food {
 }
 
 impl Food {
+
+    fn position(&self) -> &na::Point2<f32> {
+        &self.position
+    }
+
     fn random(rng: &mut dyn RngCore) -> Self {
         Self {
             position: rng.gen()
